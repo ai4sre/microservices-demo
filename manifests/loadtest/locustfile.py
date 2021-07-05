f6003cc287a45567659b828307b0891c8db49626
yuuki <yuki.tsubo@gmail.com>
2021-07-06 02:42:33 +0900

    kubernetes: support the new locust version and python3

--- manifests/loadtest/locustfile.py
+++ manifests/loadtest/locustfile.py
@@ -1,30 +1,25 @@
 import base64
+from random import choice
 
-from locust import HttpLocust, TaskSet, task
-from random import randint, choice
+from locust import HttpUser, between, task
 
 
-class WebTasks(TaskSet):
+class SockShopLoadTest(HttpUser):
+    wait_time = between(0, 0)
 
     @task
     def load(self):
-        base64string = base64.encodestring('%s:%s' % ('user', 'password')).replace('\n', '')
+        encoded = base64.b64encode(b'user:password')
 
         catalogue = self.client.get("/catalogue").json()
         category_item = choice(catalogue)
         item_id = category_item["id"]
 
         self.client.get("/")
-        self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
+        self.client.get("/login", headers={"Authorization": "Basic %s" % encoded.decode('ascii')})
         self.client.get("/category.html")
         self.client.get("/detail.html?id={}".format(item_id))
         self.client.delete("/cart")
         self.client.post("/cart", json={"id": item_id, "quantity": 1})
         self.client.get("/basket.html")
         self.client.post("/orders")
-
-
-class Web(HttpLocust):
-    task_set = WebTasks
-    min_wait = 0
-    max_wait = 0
