e9feac7c08cbb011e4b04862dde936ac71f0780c
yuuki <yuki.tsubo@gmail.com>
2021-07-27 07:26:57 +0900

    tools/diag-root-cause: use diag python API

--- tools/diag-root-cause/combination.py
+++ tools/diag-root-cause/combination.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import argparse
 import os
-import subprocess
 import sys
-from typing import Any
+from datetime import datetime
+
+import diag
 
 CUR_DIR = os.fspath(os.path.dirname(__file__))
 ALPHAS = [0.01, 0.02, 0.03, 0.04, 0.05]
@@ -11,27 +12,6 @@ ALPHAS = [0.01, 0.02, 0.03, 0.04, 0.05]
 
 def log(msg):
     print(msg, file=sys.stderr)
-
-
-def run_diag(tsdr_file: str, out_dir: str, alpha: float, pc_stable: bool) -> Any:
-    cmd = [
-        f"{CUR_DIR}/diag.py", tsdr_file,
-        '--citest-alpha', str(alpha),
-        '--out-dir', out_dir,
-    ]
-    if pc_stable:
-        cmd.append('--pc-stable')
-    log('>> ' + ' '.join(cmd))
-    proc = subprocess.run(
-            ' '.join(cmd),
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-    log(proc.stdout.decode('utf-8'))
-    log(proc.stderr.decode('utf-8'))
-    if proc.returncode != 0:
-        raise OSError('Raise an error of run_diag')
-    return proc
 
 
 def main():
@@ -42,10 +22,17 @@ def main():
     parser.add_argument("--out-dir", required=True, help="out directory")
     args = parser.parse_args()
 
+    ts = datetime.now().strftime("%Y%m%d%H%M%S")
+    dir = os.path.join(args.out_dir, ts)
+    os.makedirs(dir)
+
     for tsdr_file in args.tsdr_files:
         for pc_stable in [True, False]:
             for alpha in ALPHAS:
-                run_diag(tsdr_file, args.out_dir, alpha, pc_stable)
+                try:
+                    diag.diag(tsdr_file, alpha, pc_stable, dir)
+                except ValueError as e:
+                    log(e)
 
 
 if __name__ == '__main__':
