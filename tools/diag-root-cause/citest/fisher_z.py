288e816a3d67be656ceb7287d910fe42ff5c972a
yuuki <yuki.tsubo@gmail.com>
2021-07-27 21:26:13 +0900

    flake8

--- tools/diag-root-cause/citest/fisher_z.py
+++ tools/diag-root-cause/citest/fisher_z.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import norm
 
+
 def ci_test_fisher_z(data_matrix, x, y, s, **kwargs):
     assert 'corr_matrix' in kwargs
     cm = kwargs['corr_matrix']
@@ -8,6 +9,7 @@ def ci_test_fisher_z(data_matrix, x, y, s, **kwargs):
     z = zstat(x, y, list(s), cm, n)
     p_val = 2.0 * norm.sf(np.absolute(z))
     return p_val
+
 
 def zstat(x, y, s, cm, n):
     r = pcor_order(x, y, s, cm)
@@ -17,10 +19,12 @@ def zstat(x, y, s, cm, n):
     else:
         return zv
 
+
 def log_q1pm(r):
     if r == 1:
         r = 1 - 1e-10
     return np.log1p(2 * r / (1 - r))
+
 
 def pcor_order(x, y, s, cm):
     if len(s) == 0:
@@ -28,5 +32,3 @@ def pcor_order(x, y, s, cm):
     else:
         pim = np.linalg.pinv(cm[[x, y] + s, :][:, [x, y] + s])
         return -pim[0, 1] / np.sqrt(pim[0, 0] * pim[1, 1])
-
-
